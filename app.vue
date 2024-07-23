f533282a55cd7eb54ee82c36113304b87f37a9a0
Nikita Antonov <nikita45454@gmail.com>
2024-07-24 00:07:29 +0300

    добавляет title на страницу

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  useSeoMeta({
+    titleTemplate: (titleChunk) => {
+      return  titleChunk ? `Варк | ${titleChunk}` : 'Варк - производство трубопроводной арматуры';
+    },
+  })
+</script>
