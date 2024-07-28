3e1a088a61f09141d545dfae3812246a04309f48
Nikita Antonov <nikita45454@gmail.com>
2024-07-28 15:04:11 +0300

    feat: добавляет модуль home/main-screen

---
+++ app-modules/home/main-screen/ui/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main-screen">
+    <div class="wrapper">
+      <Header class="main-screen__header" />
+      <div class="main-screen__wrap">
+        <h1 class="h1">{{ title }}</h1>
+        <p class="main-screen__subtitle">{{ subtitle }}</p>
+        <BaseButton design="red-white-xl">ЗАКАЗАТЬ ПРОДУКЦИЮ</BaseButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import Header from '@/app-modules/header/ui/index.vue';
+
+  const title = 'ПРОИЗВОДСТВО ТРУБОПРОВОДНОЙ АРМАТУРЫ';
+  const subtitle = 'Надежность и качество в каждой детали';
+</script>
+
+<style scoped lang="scss">
+  .main-screen {
+    padding-top: 30px;
+    padding-bottom: 130px;
+    background-image: url('./assets/header.jpg');
+    background-size: cover;
+    background-position: center;
+
+    &__header {
+      margin-bottom: 80px;
+    }
+
+    &__wrap {
+      max-width: 877px;
+    }
+
+    &__subtitle {
+      margin-top: 25px;
+      margin-bottom: 55px;
+      font-weight: 500;
+      font-size: 30px;
+      line-height: 1.25;
+    }
+  }
+</style>
