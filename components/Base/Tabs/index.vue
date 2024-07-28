cadcbb6adb96b7aa5d42a3f39637391e66e5649a
Nikita Antonov <nikita45454@gmail.com>
2024-07-28 14:38:34 +0300

    feat: добавляет BaseTabs

---
+++ components/Base/Tabs/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tabs">
+    <button
+      class="tab-item"
+      v-for="(item, index) in items"
+      type="button"
+      :class="{ 'tab-item--active': activeIndex === index }"
+      @click="$emit('update:activeIndex', index)"
+    >
+      {{ item }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    items: string[],
+    activeIndex: number,
+  }>();
+
+  defineEmits<{
+    (event: 'update:activeIndex', index: number): void,
+  }>();
+</script>
+
+<style scoped lang="scss">
+  .tabs {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 2px solid #CDCDCD;
+  }
+
+  .tab-item {
+    position: relative;
+    color: #B9B9B9;
+    padding-bottom: 20px;
+    @apply tw-text-h3-bold tw-uppercase;
+
+    &--active {
+      @apply tw-text-text-dark;
+
+      &::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        left: 0;
+        bottom: -2px;
+        @apply tw-bg-text-dark;
+      }
+    }
+  }
+</style>
