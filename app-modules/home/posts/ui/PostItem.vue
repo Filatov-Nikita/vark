20e952838cd09c127eafc79d253160d53f23629d
Nikita Antonov <nikita45454@gmail.com>
2024-07-29 12:39:08 +0300

    feat: добавляет home/posts

---
+++ app-modules/home/posts/ui/PostItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <NuxtLink class="post" to="/">
+    <div class="post__img-wrap">
+      <img class="post__img" :width="post.image.width" :height="post.image.height" :src="post.image.path" :alt="post.title" />
+    </div>
+    <div class="post__date">{{ post.created_at }}</div>
+    <div class="post__title">{{ post.title }}</div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+  interface Image {
+    width: number,
+    height: number,
+    path: string,
+  }
+
+  interface Post {
+    image: Image,
+    created_at: string,
+    title: string,
+  }
+
+  defineProps<{
+    post: Post,
+  }>();
+</script>
+
+<style scoped lang="scss">
+  .post {
+    display: block;
+
+    &__img-wrap {
+      border-radius: 20px;
+      overflow: hidden;
+      height: 470px;
+    }
+
+    &__img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+
+    &__date {
+      font-size: 16px;
+      line-height: 1.25;
+      @apply tw-mt-20 tw-text-text-gray;
+    }
+
+    &__title {
+      @apply tw-mt-12 tw-text-text-dark tw-text-body-m-regular;
+    }
+  }
+</style>
---
+++ app-modules/home/posts/ui/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="posts white-area">
+    <div class="wrapper">
+      <h2 class="h2 tw-mb-10">Новости</h2>
+      <Swiper
+        :space-between="30"
+        :slides-per-view="1.7"
+        @swiper="onSlider"
+      >
+        <template v-slot:container-start>
+          <SwiperNav class="tw-mb-12 tw-justify-end"
+            @prev="swiper.slidePrev()"
+            @next="swiper.slideNext()"
+          />
+        </template>
+        <SwiperSlide>
+          <PostItem :post="post1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <PostItem :post="post1" />
+        </SwiperSlide>
+      </Swiper>
+      <div class="posts__btn">
+        <BaseButton>ПОСМОТРЕТЬ ВСЕ</BaseButton>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import PostItem from './PostItem.vue';
+  import PostImage from './assets/1.jpg';
+
+  const post1 = {
+    image: {
+      width: 1408,
+      height: 940,
+      path: PostImage,
+    },
+    created_at: '2 июня 2024',
+    title: 'ООО «ВАРК» участвовал в выставке «Газ. Нефть. Технологии» в 2024 г. Выставка проходит в ЭКСПО-центре',
+  };
+
+  const swiper = ref<any>(null);
+
+  function onSlider(_swiper: any) {
+    swiper.value = _swiper;
+  }
+</script>
+
+<style scoped lang="scss">
+  .posts {
+    padding-top: 80px;
+    padding-bottom: 120px;
+
+    &__btn {
+      margin-top: 50px;
+      text-align: center;
+    }
+  }
+</style>
