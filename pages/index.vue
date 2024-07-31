356eebbe5be6667ae415f68741596544341757c2
Nikita Antonov <nikita45454@gmail.com>
2024-07-31 13:20:14 +0300

    feat: добавляет страницу index

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    
-  </div>
+  <MainScreen />
+  <Achievements />
+  <About />
+  <Gallery />
+  <Quality />
+  <Map />
+  <Clients />
+  <Posts />
+  <OrderProductsForm />
+  <Footer />
 </template>
 
 <script lang="ts" setup>
+  import Achievements from '@/app-modules/home/achievements/ui/index.vue';
+  import Quality from '@/app-modules/home/quality/ui/index.vue';
+  import Clients from '@/app-modules/home/clients/ui/index.vue';
+  import MainScreen from '@/app-modules/home/main-screen/ui/index.vue';
+  import OrderProductsForm from '@/app-modules/order-products-form/ui/index.vue';
+  import Posts from '@/app-modules/home/posts/ui/index.vue';
+  import About from '@/app-modules/home/about/ui/index.vue';
+  import Gallery from '@/app-modules/home/gallery/ui/index.vue';
+  import Map from '@/app-modules/home/map/ui/index.vue';
+  import Footer from '@/app-modules/footer/ui/index.vue';
 
+  definePageMeta({
+    layout: false,
+  });
 </script>
 
 <style>
