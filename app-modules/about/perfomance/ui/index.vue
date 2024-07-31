48e9dfeaf53fcfa551222efe329127ebbb20220b
Nikita Antonov <nikita45454@gmail.com>
2024-08-01 00:23:52 +0300

    about/perfomance - добавляет заглушку

---
+++ app-modules/about/perfomance/ui/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="perfomance">
+    <div class="wrapper">
+      <h2 class="h2 h-mb-60"><span class="tw-text-primary">Производственные</span> мощности</h2>
+
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+  .perfomance {
+    padding-top: 90px;
+    padding-bottom: 80px;
+  }
+</style>
