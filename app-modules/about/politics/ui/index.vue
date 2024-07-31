2816c1e6ac58c24e991ac8aa1290a874817a8e28
Nikita Antonov <nikita45454@gmail.com>
2024-08-01 00:49:35 +0300

    feat: добавляет about/politics

---
+++ app-modules/about/politics/ui/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="politics white-area">
+    <div class="wrapper">
+      <h2 class="h2"><span class="tw-text-primary">Политика</span> компании</h2>
+      <p class="subtitle">{{ subtitle }}</p>
+      <Content />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import Content from './Content.vue';
+
+  const subtitle = 'в области качества, экологии, охраны здоровья и безопасности труда';
+</script>
+
+<style scoped lang="scss">
+  .politics {
+    padding-top: 90px;
+    padding-bottom: 70px;
+  }
+
+  .subtitle {
+    @apply tw-text-h3-bold tw-mb-10 tw-mb-40;
+  }
+</style>
