6df321225714785c2a95ba0c4fc5eca5c0e17b0d
Nikita Antonov <nikita45454@gmail.com>
2024-08-02 13:25:15 +0300

    feat: добавляет about/managment-system

---
+++ app-modules/about/managment-system/ui/Slider.vue
@@ -0,0 +1,48 @@
+<template>
+  <Swiper
+    :modules="[ SwiperNavigation ]"
+    :space-between="20"
+    :navigation="{
+      prevEl: '.prev-el',
+      nextEl: '.next-el',
+    }"
+  >
+    <button class="prev-el">
+      <BaseIcon name="prev" fit colorHex="#C6C6C6" />
+    </button>
+    <button class="next-el">
+      <BaseIcon name="next" fit colorHex="#C6C6C6" />
+    </button>
+    <SwiperSlide class="slide">
+      <div class="item">
+        <img class="item__img" width="984" height="1354" src="./assets/sert_1.jpg" loading="lazy" alt="Сертификат">
+      </div>
+    </SwiperSlide>
+    <SwiperSlide class="slide">
+      <div class="item">
+        <img class="item__img" width="984" height="1354" src="./assets/sert_2.jpg" loading="lazy" alt="Разрешение">
+      </div>
+    </SwiperSlide>
+    <SwiperSlide class="slide">
+      <div class="item">
+        <img class="item__img" width="984" height="1354" src="./assets/sert_3.jpg" loading="lazy" alt="Сертификат">
+      </div>
+    </SwiperSlide>
+  </Swiper>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+<style scoped lang="scss">
+  .item {
+    border-radius: 20px;
+    border: 24px solid theme('colors.white');
+  }
+
+  .slide {
+    padding: 0 48px;
+  }
+</style>
---
+++ app-modules/about/managment-system/ui/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="sys-manage gray-area">
+    <div class="wrapper">
+      <div class="wrap">
+        <div class="left">
+          <h2 class="h2 h-mb-50 title">Система менеджмента <span class="tw-text-primary">ISO&nbsp;9001</span></h2>
+          <p>{{ texts[0] }}</p>
+          <p class="text2">{{ texts[1] }}</p>
+        </div>
+        <div class="right">
+          <Slider />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import Slider from './Slider.vue';
+
+  const texts = [
+    'Качество производимой продукции контролирует внедренная интегрированная система менеджмента ISO 9001.',
+    'Это позволяет гарантировать полное соответствие требованиям российских и международных стандартов, что подтверждается действующими сертификатами.',
+  ];
+</script>
+
+<style scoped lang="scss">
+  .sys-manage {
+    padding-top: 70px;
+    padding-bottom: 140px;
+    @apply tw-text-body-m-regular;
+  }
+
+  .left {
+    width: calc(50% - 34px);
+    margin: 17px;
+
+    p + p {
+      @apply tw-mt-16;
+    }
+  }
+
+  .right {
+    width: calc(50% - 34px);
+    margin: 17px;
+  }
+
+  .wrap {
+    display: flex;
+    flex-wrap: wrap;
+    margin: -17px;
+  }
+
+  .text2 {
+    max-width: 502px;
+  }
+
+  .title {
+    max-width: 426px;
+  }
+</style>
