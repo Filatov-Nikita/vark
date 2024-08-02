d25816e98c8d9eef60a6943fbf345ba9687f2e3f
Nikita Antonov <nikita45454@gmail.com>
2024-08-02 16:31:24 +0300

    about/perfomance - добавляет элементы в заглушку

--- app-modules/about/perfomance/model/items.ts
+++ app-modules/about/perfomance/model/items.ts
@@ -1,3 +1,23 @@
-export default [
+import item1 from '../ui/assets/item1.svg';
+import item2 from '../ui/assets/item2.svg';
+import item3 from '../ui/assets/item3.svg';
+import item4 from '../ui/assets/item4.svg';
 
+export default [
+  {
+    image: item1,
+    label: 'КОНСТРУКТОРСКИЙ ОТДЕЛ',
+  },
+  {
+    image: item2,
+    label: 'аккредитованная лаборатория контроля качества',
+  },
+  {
+    image: item3,
+    label: 'Производство АРМАТУРЫ',
+  },
+  {
+    image: item4,
+    label: 'Испытательные стенды',
+  },
 ];
--- app-modules/about/perfomance/ui/index.vue
+++ app-modules/about/perfomance/ui/index.vue
@@ -2,18 +2,43 @@
   <section class="perfomance">
     <div class="wrapper">
       <h2 class="h2 h-mb-60"><span class="tw-text-primary">Производственные</span> мощности</h2>
-
+      <div class="items">
+        <div
+          v-for="item in items"
+          class="item"
+        >
+          <img class="icon" width="127" height="129" :src="item.image" loading="lazy" alt="">
+          <p class="label">{{ item.label }}</p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-
+  import items from '../model/items';
 </script>
 
 <style scoped lang="scss">
   .perfomance {
     padding-top: 90px;
     padding-bottom: 80px;
+  }
+
+  .items {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 40px;
+  }
+
+  .icon {
+    width: 127px;
+  }
+
+  .label {
+    color: #EAEAEA;
+    font-size: 22px;
+    line-height: 1.25;
+    @apply tw-mt-30 tw-uppercase tw-font-medium;
   }
 </style>
