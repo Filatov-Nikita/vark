fab2c5544e5c4fe79e76fd42cfbedd7e5598add4
Nikita Antonov <nikita45454@gmail.com>
2024-08-04 14:13:08 +0300

    feat: добавляет cтраницу about

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>about</div>
+  <MainScreen />
+  <Perfomance />
+  <Misson />
+  <Values />
+  <Politics />
+  <ManagmentSystem />
+  <Work />
 </template>
 
 <script setup lang="ts">
-
+  import MainScreen from '@/app-modules/about/main-screen/ui/index.vue';
+  import Perfomance from '@/app-modules/about/perfomance/ui/index.vue';
+  import Misson from '@/app-modules/about/misson/ui/index.vue';
+  import Politics from '@/app-modules/about/politics/ui/index.vue';
+  import ManagmentSystem from '@/app-modules/about/managment-system/ui/index.vue';
+  import Values from '@/app-modules/about/values/ui/index.vue';
+  import Work from '@/app-modules/about/work/ui/index.vue';
 </script>
