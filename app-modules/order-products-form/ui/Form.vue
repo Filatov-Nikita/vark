d460ab3e49d78fa1e3c6db5c7dd36c1ec1682ec9
Nikita Antonov <nikita45454@gmail.com>
2024-08-04 13:22:20 +0300

    refactor: выносит PolicyAccept

--- app-modules/order-products-form/ui/Form.vue
+++ app-modules/order-products-form/ui/Form.vue
@@ -2,7 +2,7 @@
   <Form class="form" :validationSchema="schema">
     <BaseInput class="form__inp" name="name" placeholder="Имя" />
     <BaseInput class="form__inp" name="email" type="email" placeholder="E-mail" />
-    <p class="form__policy">Нажимая на кнопку, вы соглашаетесь с <a :href="privacyPolicyHref" target="_blank">Политикой конфиденциальности</a></p>
+    <PolicyAccept class="form__policy" />
     <BaseButton class="form__btn" type="submit">ОТПРАВИТЬ</BaseButton>
   </Form>
 </template>
@@ -11,8 +11,6 @@
   import validationSchema from '../model/schema';
 
   const schema = validationSchema();
-
-  const { privacyPolicyHref } = storeToRefs(useContactsStore());
 </script>
 
 <style scoped lang="scss">
@@ -25,17 +23,6 @@
 
     &__policy {
       margin-top: 20px;
-      font-size: 14px;
-      line-height: 17px;
-      @apply tw-text-text-gray;
-
-      a {
-        @apply tw-underline;
-
-        &:hover {
-          @apply tw-text-white;
-        }
-      }
     }
 
     &__btn {
---
+++ components/PolicyAccept/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <p class="policy-accept" :class="[ `policy-accept--${design}`, `policy-accept--${size}` ]">Нажимая на кнопку, вы соглашаетесь с <a :href="privacyPolicyHref" target="_blank">Политикой конфиденциальности</a></p>
+</template>
+
+<script setup lang="ts">
+  withDefaults(
+    defineProps<{
+      size?: 'sm' | 'md',
+      design?: 'light' | 'dark'
+    }>(),
+    { design: 'light', size: 'md' },
+  );
+
+  const { privacyPolicyHref } = storeToRefs(useContactsStore());
+</script>
+
+<style scoped lang="scss">
+  .policy-accept {
+    @apply tw-text-text-gray;
+
+    &--md {
+      font-size: 14px;
+      line-height: 17px;
+    }
+
+    &--sm {
+      font-size: 12px;
+      line-height: 15px;
+    }
+
+    &--light {
+      a {
+        &:hover {
+          @apply tw-text-white;
+        }
+      }
+    }
+
+    &--dark {
+      a {
+        &:hover {
+          @apply tw-text-text-dark;
+        }
+      }
+    }
+
+    a {
+      @apply tw-underline;
+    }
+  }
+</style>
