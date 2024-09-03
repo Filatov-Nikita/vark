d954365fef2a3d7596eae252b0f674a8f9572589
Nikita Antonov <nikita45454@gmail.com>
2024-09-03 14:27:27 +0300

    feat: home/quality - добавляет анимацию

--- app-modules/home/quality/ui/index.vue
+++ app-modules/home/quality/ui/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="quality">
+  <section ref="section" class="quality">
     <div class="wrapper">
       <h2 class="h2 h-mb-50">Контроль качества <span class=" tw-text-primary">продукции</span></h2>
       <div class="quality__wrap">
@@ -12,7 +12,7 @@
             '--text-w': step.text_w,
           }"
         >
-          <div class="quality-item__head">
+          <div ref="items" class="quality-item__head">
             <h3>Шаг {{ index + 1 }}</h3>
           </div>
           <p class="quality-item__text" v-html="step.text"></p>
@@ -24,6 +24,22 @@
 
 <script setup lang="ts">
   import steps from '../model/steps';
+
+  const section = ref<HTMLElement | null>(null);
+  const items = ref<HTMLElement[] | null>(null);
+
+  useIntersect(
+    section,
+    () => {
+      items.value?.forEach((item, index) => {
+        setTimeout(() => {
+          item.classList.add('quality-item__head--fade');
+        }, 300 * index);
+      });
+    },
+    () => {},
+    { threshold: 0.7, once: true }
+  )
 </script>
 
 <style scoped lang="scss">
@@ -52,7 +68,14 @@
       padding: 13px 24px;
       background: var(--color);
       border-radius: 10px;
+      visibility: hidden;
       @apply tw-text-primary tw-text-body-m-bold tw-uppercase;
+
+      &--fade {
+        visibility: visible;
+        animation-name: fadeInLeft;
+        animation-duration: 500ms;
+      }
     }
 
     &__text {
