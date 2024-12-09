5f2134bb4cc71c64263544c8aaa090ac1917e34b
Nikita Antonov <nikita45454@gmail.com>
2024-12-09 15:37:54 +0500

    feat: добавляет кнопку скачать

---
+++ components/Base/Button/Download.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseButton class="show-catalog" design="outline-md" :href="href" target="_blank">
+    <span>
+      <slot></slot>
+    </span>
+    <span class="show-catalog__icon-wrap">
+      <BaseIcon fit name="download" />
+    </span>
+  </BaseButton>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    href: string,
+  }>();
+</script>
+
+<style scoped lang="scss">
+  .show-catalog {
+    display: inline-flex;
+    align-items: center;
+    gap: 10px;
+
+    &__icon-wrap {
+      width: 13px;
+      height: 13px;
+    }
+  }
+</style>
